4caddeabc96abde87114eb003cc0edb4b1e2c35a
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-06-28 22:01:11 +0530

    Q_04.js

---
+++ Q_04.js
@@ -0,0 +1,23 @@
+Q.-04-
+const findExactPath = (nums, target, start, end) => {
+     let mid =  Math.floor(start + (end - start) / 2);
+     
+     while(start < end) {
+         if(nums[mid] == target) return mid; // // found this element
+         else if(nums[mid] < target) start = mid + 1;  // search in right side of the array
+         else end = mid ;  // search in left side of the array
+
+         mid =  Math.floor(start + (end - start) / 2);
+     }
+
+     return mid;
+ }
+var searchInsert = function(nums, target) {
+    let n = nums.length;
+
+    if(n == 1 && nums[0] == target) return 0;
+
+    const path = findExactPath(nums, target, 0, n);
+
+    return path;
+};
